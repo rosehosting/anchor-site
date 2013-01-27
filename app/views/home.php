e8573b6e26b7b46e931474602661e6320f45d231
rwarasaurus <info@kieronwilson.co.uk>
2013-01-27 23:44:40 +0000

    use asset function to get base url

--- app/views/home.php
+++ app/views/home.php
@@ -2,78 +2,78 @@
 
 <div class="features wrap">
     <div class="col3">
-        <img src="/assets/img/icons/markdown.png">
+        <img src="<?php echo asset('assets/img/icons/markdown.png'); ?>">
         <h2>Markdown-happy</h2>
         <p>Anchor gives you full freedom over your words. Just write in <a href="http://daringfireball.net/projects/markdown/">Markdown</a> or HTML, whatever you prefer.</p>
     </div>
-    
+
     <div class="col3">
-        <img src="/assets/img/icons/tiny.png">
+        <img src="<?php echo asset('assets/img/icons/tiny.png'); ?>">
         <h2>Teeny-tiny</h2>
         <p>Prefer your blog systems without the bloat? Anchor weighs in smaller than a standard JPG image (~150kb).</p>
     </div>
-    
+
     <div class="col3">
-        <img src="/assets/img/icons/drag-and-drop.png">
+        <img src="<?php echo asset('assets/img/icons/drag-and-drop.png'); ?>">
         <h2>Drag-n-drop</h2>
         <p>Want to add some custom CSS, JavaScript, or an image to your post? Just drag the file over to Anchor, and we'll do the rest.</p>
     </div>
-    
+
     <div class="col3">
-        <img src="/assets/img/icons/open-source.png">
+        <img src="<?php echo asset('assets/img/icons/open-source.png'); ?>">
         <h2>Open-source</h2>
         <p>See something you want to improve in Anchor? Go right ahead; it’s all hosted on GitHub, and anyone is welcomed and invited to collaborate and contribute.</p>
     </div>
-    
+
     <div class="col3">
-        <img src="/assets/img/icons/i18n.png">
+        <img src="<?php echo asset('assets/img/icons/i18n.png'); ?>">
         <h2>Works in your language</h2>
         <p>Anchor is fully <a href="http://www.debian.org/doc/manuals/intro-i18n/">i18n</a> compatible, so you can just grab a language file and get speaking Anchor in no time.</p>
     </div>
-                
+
     <div class="col3">
-        <img src="/assets/img/icons/themes.png">
+        <img src="<?php echo asset('assets/img/icons/themes.png'); ?>">
         <h2>Super-simple theming</h2>
         <p>Creating and modifying a theme is as simple as knowing some very simple PHP and HTML/CSS. There’s no need to learn any templating languages.</p>
     </div>
-    
+
     <div class="col3">
-        <img src="/assets/img/icons/events.png">
+        <img src="<?php echo asset('assets/img/icons/events.png'); ?>">
         <h2>Event-bound</h2>
         <p>Create and utilise custom theme hooks without the need for advanced programming knowledge.</p>
     </div>
-    
+
     <div class="col3">
-        <img src="/assets/img/icons/install.png">
+        <img src="<?php echo asset('assets/img/icons/install.png'); ?>">
         <h2>Painfully easy to install</h2>
         <p>Famous five-minute install? Welcome to the tantalising two-minute install.</p>
     </div>
-    
+
     <div class="col3">
-        <img src="/assets/img/icons/extensible.png">
+        <img src="<?php echo asset('assets/img/icons/extensible.png'); ?>">
         <h2>Extensible</h2>
         <p>You can add or remove an infinite amount of custom fields to any post or page at the click of a button.</p>
     </div>
-    
+
     <div class="col3">
-        <img src="/assets/img/icons/art-directed.png">
+        <img src="<?php echo asset('assets/img/icons/art-directed.png'); ?>">
         <h2>Art-directed blogging</h2>
         <p>Make every article unique with the ability to add custom CSS, JavaScript, and HTML to any blog post or page.</p>
     </div>
-    
+
     <div class="col3">
-        <img src="/assets/img/icons/free.png">
+        <img src="<?php echo asset('assets/img/icons/free.png'); ?>">
         <h2>Forever free</h2>
         <p>Anchor has been, is, and always will be 100% free of charge to everyone.</p>
     </div>
-    
+
     <div class="col3">
-        <img src="/assets/img/icons/modern.png">
+        <img src="<?php echo asset('assets/img/icons/modern.png'); ?>">
         <h2>Markup made modern</h2>
         <p>To keep Anchor small, we only support the latest browsers &mdash; no more IE6.</p>
     </div>
 </div>
 
-<script src="/assets/js/masonry.js"></script>
+<script src="<?php echo asset('assets/js/masonry.js'); ?>"></script>
 
 <?php echo $footer; ?>
--- app/views/partials/header.php
+++ app/views/partials/header.php
@@ -5,14 +5,14 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
         <title>Anchor CMS &mdash; <?php echo $title; ?></title>
-        
+
         <meta name="description" content="Anchor is a lightweight blogging platform with a focus on simplicity and elegance.">
-        
+
         <link rel="stylesheet" href="<?php echo asset('assets/css/main.css'); ?>">
         <link rel="stylesheet" href="<?php echo asset('assets/css/small.css'); ?>" media="(max-width: 960px)">
-        
-        <link rel="shortcut icon" href="/assets/img/favicon.png">
-        
+
+        <link rel="shortcut icon" href="<?php echo asset('assets/img/favicon.png'); ?>">
+
         <meta name="HandheldFriendly" content="True">
         <meta name="MobileOptimized" content="320">
         <meta name="viewport" content="width=device-width, target-densitydpi=160dpi, initial-scale=1.0">
@@ -20,8 +20,8 @@
     <body class="<?php echo $page; ?>">
         <header id="top">
             <nav class="wrap">
-                <a href="/" title="Go to the Anchor homepage"><img src="/assets/img/logo.png" alt="Anchor CMS"></a>
-                
+                <a href="<?php echo url('home'); ?>" title="Go to the Anchor homepage"><img src="<?php echo asset('assets/img/logo.png'); ?>" alt="Anchor CMS"></a>
+
                 <ul>
                  <?php foreach(array('docs', 'forum', 'download') as $link): ?>
 	                 <?php $class = (strpos($page, $link) !== false) ? ' class="active"' : ''; ?>
@@ -29,12 +29,12 @@
                  <?php endforeach; ?>
                  </ul>
             </nav>
-            
+
             <h1 class="wrap">
                 <?php echo isset($heading) ? $heading : $title; ?>
             </h1>
-            
+
             <?php if(isset($homepage)): ?>
-            	<img class="screenie" src="/assets/img/screenshot.png">
+            	<img class="screenie" src="<?php echo asset('assets/img/screenshot.png'); ?>">
             <?php endif; ?>
 		</header>
